d020404a7d38b48baffbd43f072786b14e76e096
AriasAri <ariasari.7066@gmail.com>
2025-11-27 20:31:11 -0300

    test-inventory

---
+++ test/ui/test_inventory.py
@@ -0,0 +1,13 @@
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+
+def test_inventario(driver):
+    login = LoginPage(driver)
+    login.abrir()
+    login.login()
+
+    inventory = InventoryPage(driver)
+
+    assert inventory.obtener_titulo() == "Products"
+    assert inventory.hay_productos_visibles() is True
+    assert inventory.elementos_interfaz_presentes() is True
