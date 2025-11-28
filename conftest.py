fc31ae6c912f90f7b13aa0bcd9308f96b5e8de2a
AriasAri <ariasari.7066@gmail.com>
2025-11-27 23:38:03 -0300

    test-api

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from utils.helpers import get_driver
+import os
+from datetime import datetime
 
 @pytest.fixture
 def driver():
@@ -7,3 +9,23 @@ def driver():
     driver = get_driver()
     yield driver
     driver.quit()
+
+
+
+# Carpeta para guardar capturas
+os.makedirs("screenshots", exist_ok=True)
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # Ejecuta el test
+    outcome = yield
+    result = outcome.get_result()
+
+    # Si el test falla y tiene fixture 'driver'
+    if result.when == "call" and result.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"screenshots/{item.name}_{timestamp}.png"
+            driver.save_screenshot(filename)
+            print(f"\n[SCREENSHOT] Captura guardada en: {filename}")
---
+++ test/ui/test_login_invalido.py
@@ -0,0 +1,8 @@
+def test_login(driver):
+    driver.get("https://example.com")
+    assert False  # Forzamos fallo para probar la captura
+
+
+
+#hacer casos negativos
+
